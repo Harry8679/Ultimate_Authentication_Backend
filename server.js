28d120ae7d5042f5934811d5ba7699d249ae3c53
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-15 12:27:52 +0200

    All libraries installed

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ dotenv.config();
 
 const port = process.env.PORT || 6100;
 
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
 app.get('/api/v1/users/register', (req, res) => {
     res.send('Hello from NodeJS');
 });
 
-app.listen(() => console.log(`Server is running on the port ${port}`));
+app.listen(port, () => console.log(`Server is running on the port ${port}`));
