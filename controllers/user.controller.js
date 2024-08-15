ae6f6b7edaaae73c460aafe18006534a8d7566fd
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-15 15:22:06 +0200

    Registration of a User

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,40 @@
 const asyncHandler = require('express-async-handler');
+const User = require('../models/user.model');
 
-const home = asyncHandler(async(req, res) => {
+const home = asyncHandler(async (req, res) => {
     res.send('Server is running');
 });
 
 const register = asyncHandler(async (req, res) => {
-    console.log(req.body);
-    res.send('Registration');
+    const { name, email, password } = req.body;
+
+    // Vérifier si l'utilisateur existe déjà
+    const user = await User.findOne({ email });
+    if (user) {
+        return res.status(400).json({
+            error: 'Email is taken'
+        });
+    }
+
+    // Créer un nouvel utilisateur
+    let newUser = new User({ name, email, password });
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json({
+            message: 'User registered successfully',
+            user: {
+                id: savedUser._id,
+                name: savedUser.name,
+                email: savedUser.email
+            }
+        });
+    } catch (err) {
+        console.log('SIGNUP ERROR', err);
+        res.status(400).json({
+            error: 'Error saving the user'
+        });
+    }
 });
 
 module.exports = { home, register };
--- models/user.model.js
+++ models/user.model.js
@@ -1,67 +1,74 @@
-const { Schema, model } = require('mongoose');
-const crypto = require('crypto');
+const { Schema, model } = require("mongoose");
+const crypto = require("crypto");
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: {
-        type: String,
-        trim: true,
-        required: true,
-        max:32
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
     },
     email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-        lowercase: true
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
     },
     hashed_password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     salt: {
-        type: String
+      type: String,
     },
     role: {
-        type: String,
-        default: 'subscriber'
+      type: String,
+      default: "subscriber",
     },
     resetPasswordLink: {
-        data: String,
-        default: ''
-    }
-}, { timestamps: true });
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
 
 userSchema
-    .virtual('password')
-    .set(function(password) {
-        // Create a temporarity variable called _password
-        this._password = password;
-        // Generate salt
-        this.salt = this.encryptPassword(password);
-    })
-    .get(function() {
-        return this._password;
-    });
+  .virtual("password")
+  .set(function (password) {
+    // Create a temporarity variable called _password
+    this._password = password;
+    // Generate salt
+    this.salt = this.makeSalt(); // Generate salt
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
 
 userSchema.methods = {
-    authenticate: function(plainText) {
-        return this.encryptPassword(plainText) === this.hashed_password;
-    },
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
 
-    encryptPassword: function(password) {
-        if (!password) return '';
+  encryptPassword: function (password) {
+    if (!password) return "";
 
-        try {
-            return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
-        } catch(err) {
-            return '';
-        }
-    },
-
-    makeSalt: function() {
-        return Math.round(new Date().valueOf() * Math.random()) + '';
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
     }
+  },
+
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
 };
 
-module.exports = model('User', userSchema);
+module.exports = model("User", userSchema);
