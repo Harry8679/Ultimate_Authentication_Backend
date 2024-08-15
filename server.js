35fc94880c584148f6ae62b40ede4e7cc74e2970
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-15 13:14:43 +0200

    Connexion to Mongo Database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -7,6 +8,20 @@ dotenv.config();
 const userRoutes = require('./routes/user.route');
 
 const port = process.env.PORT || 6100;
+
+// mongoose.connect(process.env.MONGOBD_URI, {}).then(() => 'DB Connected').catch((err) => console.log('DB ERROR => ', err));
+// const connectDB = () => {
+//     mongoose
+//         .connect(process.env.MONGO_URI, {})
+//         .then(() => console.log('DB Connected !'))
+//         .catch((err) => console.log('DB Error => ', err));
+// }
+mongoose
+        .connect(process.env.MONGOBD_URI, {})
+        .then(() => console.log('DB Connected !'))
+        .catch((err) => console.log('DB Error => ', err));
+
+// console.log(process.env.MONGOBD_URI);
 
 // Middlewares
 app.use('/api/v1/users', userRoutes);
