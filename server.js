9c67d284887adf9f82949b55e9edf590be00b625
Harry EMARH <emarh.harry.code@gmail.com>
2024-08-15 13:33:26 +0200

    Middlewares added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+const morgan = require('morgan');
+const cors = require('cors');
 
 // Import Routes
 const userRoutes = require('./routes/user.route');
@@ -10,6 +12,15 @@ const connectDB = require('./config/database');
 const port = process.env.PORT || 6100;
 
 connectDB();
+// console.log(process.env.NODE_ENV);
+console.log(process.env.FRONTEND_URL);
+
+// Middlewares app
+app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'dev') {
+    app.use(cors({ origin: `${process.env.FRONTEND_URL}` }));
+}
+app.use(express.json());
 
 // Middlewares
 app.use('/api/v1/users', userRoutes);
